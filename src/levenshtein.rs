2f9200641a3a207e55d32ba735e0e6051b270157
James Turk <dev@jamesturk.net>
2020-06-28 19:33:40 -0400

    damerau levenshtein

--- src/levenshtein.rs
+++ src/levenshtein.rs
@@ -1,4 +1,5 @@
 use std::cmp;
+use std::collections::HashMap;
 use unicode_segmentation::UnicodeSegmentation;
 
 fn range_vec(size: usize) -> Vec<usize> {
@@ -40,6 +41,56 @@ pub fn vec_levenshtein_distance<T: PartialEq>(v1: &Vec<T>, v2: &Vec<T>) -> usize
     return cur[cur.len() - 1];
 }
 
+pub fn vec_damerau_levenshtein_distance<T: Eq + std::hash::Hash>(
+    v1: &Vec<T>,
+    v2: &Vec<T>,
+) -> usize {
+    let len1 = v1.len();
+    let len2 = v2.len();
+    let infinite = len1 + len2;
+
+    let mut item_position = HashMap::new();
+    // distance matrix
+    let mut score = vec![vec![0; len2 + 2]; len1 + 2];
+    score[0][0] = infinite;
+    for i in 0..len1 + 1 {
+        score[i + 1][0] = infinite;
+        score[i + 1][1] = i;
+    }
+    for i in 0..len2 + 1 {
+        score[0][i + 1] = infinite;
+        score[1][i + 1] = i;
+    }
+
+    for i in 1..len1 + 1 {
+        let mut db = 0;
+        for j in 1..len2 + 1 {
+            let i1 = item_position.entry(&v2[j - 1]).or_insert(0);
+            let j1 = db;
+            let mut cost = 1;
+            if v1[i - 1] == v2[j - 1] {
+                cost = 0;
+                db = j;
+            }
+
+            // min of the four options
+            score[i + 1][j + 1] = cmp::min(
+                // substitution & insertion
+                cmp::min(score[i][j] + cost, score[i + 1][j] + 1),
+                cmp::min(
+                    // deletion & transposition
+                    score[i][j + 1] + 1,
+                    score[*i1][j1] + (i - *i1 - 1) + 1 + (j - j1 - 1),
+                ),
+            )
+        }
+        // store the position of this character for transpositions
+        item_position.insert(&v1[i - 1], i);
+    }
+
+    return score[len1 + 1][len2 + 1];
+}
+
 pub fn levenshtein_distance(s1: &str, s2: &str) -> usize {
     if s1 == s2 {
         return 0;
@@ -51,6 +102,17 @@ pub fn levenshtein_distance(s1: &str, s2: &str) -> usize {
     vec_levenshtein_distance(&us1, &us2)
 }
 
+pub fn damerau_levenshtein_distance(s1: &str, s2: &str) -> usize {
+    if s1 == s2 {
+        return 0;
+    }
+
+    let us1 = UnicodeSegmentation::graphemes(s1, true).collect::<Vec<&str>>();
+    let us2 = UnicodeSegmentation::graphemes(s2, true).collect::<Vec<&str>>();
+
+    vec_damerau_levenshtein_distance(&us1, &us2)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -58,5 +120,10 @@ mod test {
     #[test]
     fn test_levenshtein() {
         testutils::test_distance_func("testdata/levenshtein.csv", levenshtein_distance);
+    }
+
+    #[test]
+    fn test_damerau_levenshtein() {
+        testutils::test_distance_func("testdata/damerau_levenshtein.csv", damerau_levenshtein_distance);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,8 @@ pub use jaro::{
     jaro_similarity, jaro_winkler_similarity, jaro_winkler_similarity_longtol, vec_jaro_similarity,
     vec_jaro_winkler_similarity, vec_jaro_winkler_similarity_longtol,
 };
-pub use levenshtein::levenshtein_distance;
+pub use levenshtein::{
+    damerau_levenshtein_distance, levenshtein_distance, vec_damerau_levenshtein_distance,
+    vec_levenshtein_distance,
+};
 mod testutils;
